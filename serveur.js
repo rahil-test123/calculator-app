44b0c527690389350aa63af660a64296c5021651
rahil-test123 <rahil.msouhli123@gmail.com>
2026-03-08 22:01:53 +0100

    test auto build

--- serveur.js
+++ serveur.js
@@ -1,19 +1,20 @@
-{
-  "name": "calculator-app",
-  "version": "1.0.0",
-  "description": "",
-  "main": "serveur.js",
-  "scripts": {
-    "start": "node serveur.js",
-    "test": "echo Tests OK"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
-  "type": "commonjs",
-  "dependencies": {
-    "express": "^5.1.0"
-  }
-}
-// test webhook// test auto build  
-// test webhook again  
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+// route principale
+app.get('/', (req, res) => {
+  res.send('Serveur calculator-app en marche');
+});
+
+// route webhook
+app.post('/webhook', (req, res) => {
+  console.log('Webhook reçu');
+  res.send('OK');
+});
+
+// démarrage du serveur
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
